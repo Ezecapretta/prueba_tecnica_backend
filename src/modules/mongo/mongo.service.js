fca59058a1a0f36d41d79e1446c9d7f9043062e7
Eze Capretta <caprettaeze@gmail.com>
2024-04-05 18:46:48 -0300

    Update mongo.service.js

--- src/modules/mongo/mongo.service.js
+++ src/modules/mongo/mongo.service.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const URI = "mongodb://localhost:27017/product-db";
+// const URI = "mongodb://localhost:27017/product-db";
+const URI =
+  "mongodb+srv://admin23:QJzJbFBXWCUWSc7E@cluster0.xmu3qzl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectToMongoDB = async () => {
   try {
